f735e806e1ee7afafab4325ac0001c7b9f8ff2ad
VerzaElisa <elisaverza.gm@gmail.com>
2023-08-17 13:20:13 +0200

    Finiti test write

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/ReadWriteMoveTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/ReadWriteMoveTest.java
@@ -31,6 +31,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Random;
 import java.util.Set;
 
 import org.mockito.Answers;
@@ -58,27 +59,36 @@ public class ReadWriteMoveTest{
     private int toSum;
     private boolean bestEffort;
     private ByteBuffer bb;
+    private ByteBuffer[] writeBbArray;
     private long exp;
+    private long writePos;
+    private int wBbSize;
+    private Integer writeBuffArray;
+    private String wExcept;
 
     @Parameters
     public static Collection<Object[]> getTestParameters(){
         return Arrays.asList(new Object[][]{
-//          | exception                  | start     | bbSize  | bestEffort |
-            { "IllegalArgumentException" , -1025     , -1      , true       }, 
-            { null                       , fileLen+1 , -1      , true       },    
-            { null                       , -1024     , 0       , true       },
-            { "ShortReadException"       , -1024     , 0       , false      }, 
-            { null                       , -1024     , -1      , true       },
-            { null                       , fileLen   , +1      , true       },  
-            { "ShortReadException"       , fileLen   , +1      , false      },   
+//          | exception                  | start     | bbSize  | bestEffort | writeBuffArray | writePos | wBbSize       | wExcept                          |
+            { "IllegalArgumentException" , -1025     , -1      , true       , 1              , -1025     , -1025-fileLen , "IllegalArgumentException"       }, 
+            { null                       , fileLen+1 , -1      , true       , null           , 0         , 0             , "NullPointerException"           },    
+            { null                       , -1024     , 0       , true       , 0              , 0         , 0             , "ArrayIndexOutOfBoundsException" },
+            { "ShortReadException"       , -1024     , 0       , false      , 1              , 0         , 0             , "ShortWriteException"            }, 
+            { null                       , -1024     , -1      , true       , 1              , 0         , fileLen       , null                             },
+            { null                       , fileLen   , +1      , true       , 1              , 0         , fileLen+1     , null                             },  
+            { "ShortReadException"       , fileLen   , +1      , false      , 1              , fileLen+1 , 1             , null                             },   
         });
     }
 
-    public ReadWriteMoveTest(String exception, int start, int toSum, boolean bestEffort){
+    public ReadWriteMoveTest(String exception, int start, int toSum, boolean bestEffort, Integer writeBuffArray, long writePos, int wBbSize, String wExcept){
         this.exception = exception;
         this.start = start;
         this.toSum = toSum;
         this.bestEffort = bestEffort;
+        this.writePos = writePos;
+        this.wBbSize = wBbSize;
+        this.wExcept = wExcept;
+        this.writeBuffArray = writeBuffArray;
     }
 
 /*Nel setup viene creato l'oggetto FileInfo.*/
@@ -95,7 +105,26 @@ public class ReadWriteMoveTest{
         bb.rewind();
         //Viene assegnato il valore atteso in caso di successo
         exp = Math.min(Math.abs(start-fileLen), bb.capacity());
+        
     }
+
+    @Before
+    public void writeSetUp(){
+        if(writeBuffArray != null){
+            writeBbArray = new ByteBuffer[(int) writeBuffArray];
+            if(writeBuffArray > 0){
+                //Viene creato un buffer da inserire nel byte buffer array
+                writeBbArray[0] = ByteBuffer.allocate(Math.abs(wBbSize));
+                writeBbArray[0].rewind();
+                Random random = new Random();
+                byte[] fillBuff = new byte[Math.abs(wBbSize)];
+                random.nextBytes(fillBuff);
+                writeBbArray[0].put(fillBuff);
+                writeBbArray[0].rewind();
+            }
+        } 
+    }
+
     @After
     public void onClose(){
         File myObj = new File(Variables.LEDGER_FILE_INDEX); 
@@ -108,6 +137,17 @@ public class ReadWriteMoveTest{
             Assert.assertEquals(exp, ret);
         }catch(Exception e){    
             Assert.assertEquals(exception, e.getClass().getSimpleName());
+        }
+    }
+
+    @Test
+    public void writeTest(){
+         try{
+            long ret = fi.write(writeBbArray, writePos);
+            Assert.assertEquals(wBbSize, ret);
+            Assert.assertEquals(Math.max(fileLen+1024, writePos+wBbSize+1024), fl.length());
+        }catch(Exception e){    
+            Assert.assertEquals(wExcept, e.getClass().getSimpleName());
         }
     }
 }
