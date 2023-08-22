57e3ad07bad42b1fd8d0e9cfc88ce8afa6484b0a
VerzaElisa <elisaverza.gm@gmail.com>
2023-08-22 16:02:38 +0200

    Coverage waitLasAddConfirmed

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/SetAndWaitLACTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/SetAndWaitLACTest.java
@@ -3,7 +3,10 @@ package org.apache.bookkeeper.bookie;
 import org.apache.bookkeeper.common.util.Watcher;
 import org.apache.bookkeeper.net.BookieId;
 import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.appender.FileAppender;
 import org.apache.logging.log4j.core.config.Configurator;
+import org.apache.logging.log4j.core.config.LoggerConfig;
+import org.apache.logging.log4j.core.layout.PatternLayout;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -14,6 +17,7 @@ import org.junit.runners.Parameterized.Parameters;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
@@ -22,6 +26,7 @@ import static org.mockito.Mockito.when;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -33,6 +38,13 @@ import org.mockito.Answers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.stubbing.Answer;
+import org.slf4j.LoggerFactory;
+import org.slf4j.event.SubstituteLoggingEvent;
+import org.slf4j.impl.StaticLoggerBinder;
+import org.slf4j.Logger;
+import org.slf4j.event.LoggingEvent;
+
+
 
 @RunWith(value=Parameterized.class)
 public class SetAndWaitLACTest{
@@ -50,6 +62,7 @@ public class SetAndWaitLACTest{
 
     private long lac;
     private String lacStr;
+    private Level logStatus;
     private String elem = "";
     private List<Long> parsed;
     private List<Long> exp;
@@ -58,36 +71,63 @@ public class SetAndWaitLACTest{
     private long cur;
     private String exception;
     private long prevLac;
+    private Logger LOG;
+    private Logger LOG_SPY;
+    private boolean isClosed;
+
 
     @Parameters
     public static Collection<Object[]> getTestParameters(){
         return Arrays.asList(new Object[][]{
-//          | LAC | LAC list | exception              | watcher | wait  |
-            {-1   , "3 2 4"  , null                   , watcher , false }, 
-            {0    , "3 2 4"  , null                   , watcher , true  },
-            {1    , "3 2 4"  , "NullPointerException" , null    , true  },
+//          | LAC | LAC list | exception              | watcher | wait  | logStatus   | isClosed |
+            {-1   , "3 2 4"  , null                   , watcher , false , Level.TRACE , false    }, 
+            {0    , "3 2 4"  , null                   , watcher , true  , Level.INFO  , false    },
+            {1    , "3 2 4"  , "NullPointerException" , null    , true  , Level.INFO  , false    },
+            {0    , ""       , null                   , watcher , false , Level.INFO  , true     },
+            //{null , null     , null                   , watcher , false , Level.TRACE , true     },
         });
     }
 
-    public SetAndWaitLACTest(long lac, String lacStr, String exception, Watcher<LastAddConfirmedUpdateNotification> watcher, boolean wait){
+    public SetAndWaitLACTest(long lac, String lacStr, String exception, Watcher<LastAddConfirmedUpdateNotification> watcher, boolean wait, Level logStatus, boolean isClosed){
         this.lac = lac;
         this.lacStr = lacStr;
         SetAndWaitLACTest.watcher = watcher;
         this.wait = wait;
         this.exception = exception;
+        this.logStatus = logStatus;
+        this.isClosed = isClosed;
     }
 
 /*Nel setup viene creato l'oggetto FileInfo.*/
     @Before
-    public void setUp() throws IOException {
-        Configurator.setLevel("org.apache.bookkeeper.bookie.FileInfo", Level.TRACE);
-        parsed = new ArrayList<Long>();
-        exp = new ArrayList<Long>();
+    public void setUp() throws Exception {
+        Configurator.setLevel("org.apache.bookkeeper.bookie.FileInfo", logStatus);
         byte[] mk = Variables.MASTER_KEY.getBytes();;
         fl = new File(Variables.LEDGER_FILE_INDEX);
         int ver = Variables.VERSION;
-        fi = new FileInfo(fl, mk, ver);    
+
+        //Inserimento logger spy
+        LOG = LoggerFactory.getLogger(FileInfo.class);
+        LOG_SPY = spy(LOG);
+        Utilities.setFinalStatic(FileInfo.class.getDeclaredField("LOG"), LOG_SPY);
+
+        fi = new FileInfo(fl, mk, ver);   
+         
         //Creazione lista invocazioni successive di setLasAddConfirmed
+        if(!lacStr.equals("")){
+            expBuilding();
+        }
+        //Creato mock per waitForLastAddConfirmedUpdate
+        watcher = Mockito.mock(Watcher.class);
+
+        //Modifica parametro isClosed
+        Utilities.setPrivate(fi, isClosed, "isClosed");
+    }
+
+    public void expBuilding(){
+        parsed = new ArrayList<Long>();
+        exp = new ArrayList<Long>();
+
         parsed = new ArrayList<Long>();
         String[] lacList = lacStr.split(" ");
         for(String i : lacList){
@@ -107,9 +147,10 @@ public class SetAndWaitLACTest{
         if(!wait){
             prevLac--;
         }
-        //Creato mock per waitForLastAddConfirmedUpdate
-        watcher = Mockito.mock(Watcher.class);
     }
+
+    
+
     @After
     public void onClose(){
         File myObj = new File(Variables.LEDGER_FILE_INDEX); 
@@ -117,12 +158,22 @@ public class SetAndWaitLACTest{
     }
     @Test
     public void SAndWLACTest() throws IOException {
-        assertEquals(lac, fi.setLastAddConfirmed(lac));
-        for(int i = 0; i<parsed.size(); i++){
-            assertEquals((long)exp.get(i), fi.setLastAddConfirmed(parsed.get(i)));
+        if(!lacStr.equals("")){
+            long ret = fi.setLastAddConfirmed(lac);
+            assertEquals(lac, ret);
+            for(int i = 0; i<parsed.size(); i++){
+                assertEquals((long)exp.get(i), fi.setLastAddConfirmed(parsed.get(i)));
+                if(logStatus.equals(Level.TRACE)){
+                    verify(LOG_SPY).trace("Updating LAC {} , {}", exp.get(i), parsed.get(i));
+                }
+            }
         }
         try{
             assertEquals(wait, fi.waitForLastAddConfirmedUpdate(prevLac, watcher));
+            if(logStatus.equals(Level.TRACE)){
+                verify(LOG_SPY).trace("Wait For LAC {} , {}", exp.get(exp.size()-1), prevLac);
+            }
+
         }catch(Exception e){    
             Assert.assertEquals(exception, e.getClass().getSimpleName());
         }
--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/Utilities.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/Utilities.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.security.NoSuchAlgorithmException;
@@ -15,6 +17,20 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 
 
 public class Utilities {
+    
+    public static void setFinalStatic(Field field, Object newValue) throws Exception {
+        field.setAccessible(true);        
+        Field modifiersField = Field.class.getDeclaredField("modifiers");
+        modifiersField.setAccessible(true);
+        modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+        field.set(null, newValue);
+    }
+
+    public static void setPrivate(Object classToModify, Object newValue, String fieldName) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException{
+        Field privateField = classToModify.getClass().getDeclaredField(fieldName);
+        privateField.setAccessible(true);
+        privateField.set(classToModify, newValue);  
+    }
 
     public static void createFile(File fl, String key, String magic, int version, int headerMKLen, int buffLen) throws FileNotFoundException{
         try (FileChannel myWriter = new RandomAccessFile(fl, "rw").getChannel()) {
